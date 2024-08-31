47eebc8e1137a9a0f4443dedd88edfce96ccef8e
troywein <troywein@gmail.com>
2024-08-31 09:27:29 -0600

    updated with file header guard

--- quiz03functs.h
+++ quiz03functs.h
@@ -5,9 +5,13 @@
 * Purpose: Quiz 3 
 * ===========================================================
 */
+#ifndef QUIZ3_H
+#define QUIZ3_H
  
  #define RISK_LEVEL_LOW  0
  #define RISK_LEVEL_MID  1
  #define RISK_LEVEL_HIGH 2 
  
 int getRiskLevel(int riskPoints, int numCrewMembers);
+
+#endif //QUIZ3_H
